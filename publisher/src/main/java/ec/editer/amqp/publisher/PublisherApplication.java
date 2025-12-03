94a48d7127a3969cfdbb880e5eca76de38937b7d
Edison Teran <javaedi.et@gmail.com>
2025-12-03 10:25:09 -0500

    add scheduler for available and not available books

--- publisher/src/main/java/ec/editer/amqp/publisher/PublisherApplication.java
+++ publisher/src/main/java/ec/editer/amqp/publisher/PublisherApplication.java
@@ -2,8 +2,10 @@ package ec.editer.amqp.publisher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PublisherApplication {
 
 	public static void main(String[] args) {
